840c673a6c6a877b87466f76edb3d7f38bd177d6
yKlym <yaroslaw.klymko@gmail.com>
2020-04-06 21:04:49 +0300

    lab5 done

---
+++ lab5/script.js
@@ -0,0 +1,72 @@
+(function(){
+
+    const imageWrapper = document.querySelector(".content-wrapper"),
+    loaderDiv = document.querySelector(".loader"),
+    apiUrl = "https://randomuser.me/api/";
+
+window.onload = () => {
+    makeRequest(apiUrl, 50).then((httpRequest) => {
+        renderContent(httpRequest);
+    })
+};
+
+function makeRequest(url, peopleCount) {
+    let httpRequest = false;
+    return new Promise(function (resolve, reject) {
+        if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+            httpRequest = new XMLHttpRequest();
+            if (httpRequest.overrideMimeType) {
+                httpRequest.overrideMimeType('text/xml'); // For Mozila old versions
+            }
+        } else if (window.ActiveXObject) { // IE
+            try {
+                httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch (e) {
+                try {
+                    httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+                } catch (e) {
+                    console.log(`Error : ${e}`);
+                }
+            }
+        }
+
+        httpRequest.onload = function () {
+            resolve(httpRequest);
+        };
+
+        httpRequest.open('GET', `${url}?results=${peopleCount || 1}`, true);
+        httpRequest.send(null);
+
+    });
+}
+
+function renderContent(httpRequest) {
+    const results = JSON.parse(httpRequest.responseText).results;
+    for (let res of results) {
+        let newImg = document.createElement("img");
+        newImg.setAttribute("src", res.picture.large);
+        imageWrapper.appendChild(newImg);
+
+    }
+}
+
+document.addEventListener("scroll", _.throttle((event) => {
+
+    console.log(pageYOffset);
+    if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
+        loaderDiv.style.display = "flex";
+        loaderDiv.classList.add("show");
+        makeRequest(apiUrl, 25).then((httpRequest) => {
+
+            return renderContent(httpRequest);
+        }).then(() => {
+            console.log(`New height: ${parseInt(getComputedStyle(imageWrapper).height)}`);
+            console.log("---------------------------<");
+            setTimeout(() => {
+                loaderDiv.classList.remove("show");
+            }, 1000);
+        });
+
+    }
+}, 500));
+})()
